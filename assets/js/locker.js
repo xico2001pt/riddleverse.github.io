39eae4d3cd6d442547ad084c512f220ec1901e82
xico2001pt <xico2001pt@users.noreply.github.com>
2024-10-03 22:09:46 +0000

    deploy: 7640bf51fe6f5731c5e956b32f9fd7d304ae5354

---
+++ assets/js/locker.js
@@ -0,0 +1,36 @@
+import { getName, getProgress, containsStory, getProgressLength, updateProfile } from "./profile.js";
+
+function lockElement(element) {
+    // Check if the element is an image
+    if (element.tagName === 'img') {
+        // Change the image src to a placeholder
+        element.src = '/assets/img/placeholder.jpg'; // Placeholder image URL
+    } 
+    // Check if the element has children
+    else if (element.children && element.children.length > 0) {
+        // Recursively call the function for each child
+        for (let i = 0; i < element.children.length; i++) {
+            lockElement(element.children[i]);
+        }
+    }
+    // If the element is neither an image nor has children, change its value
+    else {
+        // Change the value or text content to "Locked"
+        if (element.tagName === 'input' || element.tagName === 'textarea') {
+            element.value = 'Locked';
+        } else {
+            element.textContent = 'Locked';
+        }
+        if (element.href) {
+            // Change the image src to a placeholder
+            element.href = '';
+        } 
+    }
+}
+
+export function updateLockbyLevel(elemId, story, minLevel) {
+    let currLevel = getProgress(story);
+    if (currLevel < minLevel) {
+        lockElement(document.getElementById(elemId));
+    }
+}

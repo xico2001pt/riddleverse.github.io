d9d6a85c87cec7f33a70920643821b3a9acf5498
xico2001pt <xico2001pt@users.noreply.github.com>
2024-10-05 14:41:20 +0000

    deploy: ff3ddbe46ebc4df4c28b43c9b88df3c33ab8d810

--- assets/js/stories.js
+++ assets/js/stories.js
@@ -3,6 +3,9 @@ export const STORIES_DATA = [
         story: 'christmas2024',
         title: 'The Lost Artifacts of Christmas',
         total_levels: 24,
+        solutions: [
+            "Fire",
+        ]
     },
     {
         story: 'christmas2025',
@@ -10,3 +13,11 @@ export const STORIES_DATA = [
         total_levels: 24,
     }
 ];
+
+export function getStoryData(story) {
+    for (let i = 0; i < STORIES_DATA.length; ++i) {
+        if (STORIES_DATA[i].story === story)
+            return STORIES_DATA[i];
+    }
+    return null;
+}

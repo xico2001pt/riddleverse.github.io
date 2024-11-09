9d335f63c81f304d9206d1e8b16c8e6638741291
Francisco Cerqueira <franciscogc@outlook.pt>
2024-11-09 22:23:22 +0000

    chore: add alerts for profile actions

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -1,27 +1,23 @@
-const IV = new TextEncoder().encode("francisco");
-
-let storageKey = "cerqueira";
-
 export function readFromStorage(key) {
     let content = localStorage.getItem(key);
     if (content) {
-        content = decodeString(content, storageKey);
+        content = decodeString(content);
     }
     return content;
 }
 
 export function writeToStorage(key, value) {
-    value = encodeString(value, storageKey);
+    value = encodeString(value);
     localStorage.setItem(key, value);
 }
 
 function encodeString(input) {
     const encoder = new TextEncoder();
     return btoa(String.fromCharCode(...encoder.encode(input)));
-  }
-  
+}
+
 function decodeString(encoded) {
-const decoder = new TextDecoder();
-const decodedData = atob(encoded).split('').map(char => char.charCodeAt(0));
-return decoder.decode(new Uint8Array(decodedData));
+    const decoder = new TextDecoder();
+    const decodedData = atob(encoded).split('').map(char => char.charCodeAt(0));
+    return decoder.decode(new Uint8Array(decodedData));
 }

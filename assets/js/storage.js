de4973794f85721ce2265e7242edfd27201e5aa4
xico2001pt <xico2001pt@users.noreply.github.com>
2024-11-10 10:28:49 +0000

    deploy: 80e01497d7a7ee00b4107c86e8eb5e4e651ed74c

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -33,15 +33,3 @@ export function downloadFile(name, content) {
 
     URL.revokeObjectURL(link.href);
 }
-
-export function downloadFile(name, content) {
-    const blob = new Blob([content], { type: 'text/plain' });
-            
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = name;
-
-    link.click();
-
-    URL.revokeObjectURL(link.href);
-}
